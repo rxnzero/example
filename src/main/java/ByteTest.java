861384f543c573b043e4417643dc43cb45ed6bf6
rxnzero <rxnzero#gmail.com>
2018-08-20 10:59:43 +0900

    add byte test code

---
+++ src/main/java/ByteTest.java
@@ -0,0 +1,15 @@
+
+public class ByteTest {
+
+	public ByteTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte[] bytes = {0x31, 0x30,0x30,0x30};
+		
+		System.out.println(new String(bytes) );
+	}
+
+}
